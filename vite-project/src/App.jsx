8521b31c808fd9c91da66a0622eca6a6dcd6d76d
abhishekshilu978 <abhishekkumar1087@gmail.com>
2025-10-31 15:24:19 +0530

    3rd Changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+    const [countries, setCountries] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    useEffect(() => {
+        fetch(
+            "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries"
+        )
+        .then((res) => res.json())
+        .then((data) => {
+            setCountries(data);
+        })
+        .catch((error) => {
+            console.error("Error fetching countries:", error);
+        });
+        },[]
+        );
+        const filteredCountries = countries.filter((country) => 
+            country.common.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        return (
+            <div style={{ padding: "20px" }}>
+                <input type="text" 
+                placeholder="Search for countries..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                style={{
+                    width: "100%",
+                    // maxWidth: "600px",
+                    padding: "10px",
+                    fontSize: "16px",
+                    marginBottom: "20px",
+                    // paddingLeft: "25%"
+                }}
+                />
+                <div
+                style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    gap: "16px",
+                    justifyContent: "center",
+                }}
+                >
+                    {filteredCountries.map((country, index) => (
+                        <div
+                        className="countryCard"
+                        key={country.common + index}
+                        style={{
+                            border: "1px solid #ddd",
+                            borderRadius: "8px",
+                            padding: "10px",
+                            textAlign: "center",
+                            width: "150px",
+                            boxShadow: "0 2px 5px rgba(0,0,0,0.1)",
+                        }}
+                        >
+                            <img src={country.png} 
+                            alt={country.common}
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                objectFit: "contain",
+                                marginBottom: "10px",
+                            }}
+                             />
+                             <p
+                             style={{
+                                fontWeight: "bold"
+                             }}
+                             >
+                                {country.common}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                             </p>
+
+                        </div>
+                    ))}
+
+                </div>
+
+            </div>
+        );
 }
-
-export default App
+export default App;
